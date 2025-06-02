3d3f782c379bdcdf166e0b6d364527746325e595
eduardomodolo <uniceantunes@fei.edu.br>
2025-06-02 08:18:04 -0300

    estudos

--- Estudo/estudos.py
+++ Estudo/estudos.py
@@ -1,7 +1,8 @@
-n1 = int(input("Digite o primeiro número:"))
-n2 = int(input("Digite o segundo número:"))
+n1 = int(input("Digite o primeiro numero:"))
+n2 = int(input("Digite o segundo numero:"))
 
-if n1 > n2:
-    print("O maior número é:", n1)
-else:
-    print("O maior número é:",n2)
+def calcular_media(n1, n2):
+    media = (n1 + n2) / 2
+    print(f"A media entre {n1} e {n2} e {media}.")
+
+calcular_media(n1, n2)
