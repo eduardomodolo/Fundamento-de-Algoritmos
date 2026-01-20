72dab17de6c1c3193b219500635747f5181b354f
eduardomodolo <uniceantunes@fei.edu.br>
2026-01-19 21:21:50 -0300

    Delete Estudo directory

--- Estudo/estudos.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input("Digite o primeiro numero:"))
-n2 = int(input("Digite o segundo numero:"))
-
-def calcular_media(n1, n2):
-    media = (n1 + n2) / 2
-    print(f"A media entre {n1} e {n2} e {media}.")
-
-calcular_media(n1, n2)
